bf0d775e17d948e9b53abac329bd44a47b7020c9
Matue777x666 <matheusemail222@gmail.com>
2026-04-12 12:03:39 -0300

    LiaV1

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const { Client, GatewayIntentBits } = require('discord.js')
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+client.on('ready', () => {
+  console.log('Lia está online!')
+})
+
+client.on('messageCreate', message => {
+  if(message.author.bot) return
+
+  if(message.content.toLowerCase().includes('lia')){
+    message.reply('Oi! Sou a Lia , Suporte N2')
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
